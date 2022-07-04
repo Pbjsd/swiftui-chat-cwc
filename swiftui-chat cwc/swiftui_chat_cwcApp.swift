7c9d63cdbdb3f86cd15c7c53fa513ecbd3cc992b
Sandeep Dhaliwal <Sandeep.k.dhaliwal1@gmail.com>
2022-07-04 14:28:41 -0400

    Initial Commit

---
+++ swiftui-chat cwc/swiftui_chat_cwcApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_chat_cwcApp.swift
+//  swiftui-chat cwc
+//
+//  Created by Panchi on 7/4/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_chat_cwcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
