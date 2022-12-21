6a817a356b004de0fa323bb3d8ad52a5f5304a8f
Sandeep Dhaliwal <Sandeep.k.dhaliwal1@gmail.com>
2022-12-21 14:30:47 -0500

    12/21/22: Uploading Profile Image

--- swiftui-chat cwc/Views/Onboarding/CreateProfileView.swift
+++ swiftui-chat cwc/Views/Onboarding/CreateProfileView.swift
@@ -14,6 +14,14 @@ struct CreateProfileView: View {
     @State var firstName = ""
     @State var lastName = ""
     
+    @State var selectedImage: UIImage?
+    @State var isPickerShowing = false
+    
+    @State var isSourceMenuShowing = false
+    @State var source: UIImagePickerController.SourceType = .photoLibrary
+    
+    @State var isSaveButtonDisabled = false
+    
     var body: some View {
         
         VStack {
@@ -32,19 +40,28 @@ struct CreateProfileView: View {
             Button {
                 
                 // Show action sheet
+                isSourceMenuShowing = true
                 
             } label: {
                 
                 ZStack {
                     
-                    Circle()
-                        .foregroundColor(Color.white)
+                    if selectedImage != nil {
+                        Image(uiImage: selectedImage!)
+                            .resizable()
+                            .scaledToFill()
+                            .clipShape(Circle())
+                    }
+                    else {
+                        Circle()
+                            .foregroundColor(Color.white)
+                        
+                        Image(systemName: "camera.fill")
+                            .tint(Color("icons-input"))
+                }
                     
-                    Circle()
-                        .stroke(Color("create-profile-border"), lineWidth: 2)
-                    
-                    Image(systemName: "camera.fill")
-                        .tint(Color("icons-input"))
+                Circle()
+                    .stroke(Color("create-profile-border"), lineWidth: 2)
                     
                 }
                 .frame(width: 134, height: 134)
@@ -59,27 +76,77 @@ struct CreateProfileView: View {
             
             
             // Last name
-            TextField("Last Name", text: $firstName)
+            TextField("Last Name", text: $lastName)
                 .textFieldStyle(CreateProfileTextfieldStyle())
             
             Spacer()
             
             Button {
-                // Next step
+              
+                // Prevent double taps
+                isSaveButtonDisabled = true
                 
-                currentStep = .contacts
-                
+                // Save the data
+                DatabaseService().setUserProfile(firstName: firstName,
+                                                 lastName: lastName,
+                                                 image: selectedImage) { isSuccess in
+                    if isSuccess {
+                        currentStep = .contacts
+                    }
+                    else {
+                        // TODO: Show error message to the user
+                    }
+                    
+                    isSaveButtonDisabled = false
+                }
+          
             } label: {
-                Text("Next")
+                Text(isSaveButtonDisabled ? "Uploading" : "Save")
             }
             .buttonStyle(OnboardingButtonStyle())
+            .disabled(isSaveButtonDisabled)
             .padding(.bottom, 87)
             
         
-            }
+          }
             .padding(.horizontal)
-    
+            .confirmationDialog("From where?", isPresented: $isSourceMenuShowing, actions: {
+                
+                Button {
+                    // Set the source to photo library
+                    self.source = .photoLibrary
+                    
+                    // Show the image picker
+                    isPickerShowing = true
+                    
+                } label: {
+                    Text("Photo Library")
+                }
+                
+                if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                
+                    Button {
+                        // Set the source to camera
+                        self.source = .camera
+                        
+                        // Show the image picker
+                        isPickerShowing = true
+                    } label: {
+                        Text("Take Photo")
+                    }
+                }
+
+                
+            })
+            .sheet(isPresented: $isPickerShowing) {
+                
+                // Show the image picker
+                ImagePicker(selectedImage: $selectedImage,
+                            isPickerShowing: $isPickerShowing, source: self.source)
+        }
+        
     }
+}
 
 struct CreateProfileView_Previews: PreviewProvider {
     static var previews: some View {
@@ -87,4 +154,4 @@ struct CreateProfileView_Previews: PreviewProvider {
     }
 }
     
-}
+
